858db1419fa0c9254fbdfc4fa107da5e96493475
anneraev <47757104+anneraev@users.noreply.github.com>
2019-10-27 05:05:54 -0500

    set state working

--- src/Headline/Headline.js
+++ src/Headline/Headline.js
@@ -4,10 +4,24 @@ import React, {useState} from 'react';
 //     return <h1>{value}</h1>;
 // };
 
+// const Headline = () => {
+//     const [greeting, setGreeting] = useState('Function Component Use State!')
+
+//     return <h1>{greeting}</h1>
+// };
+
 const Headline = () => {
     const [greeting, setGreeting] = useState('Function Component Use State!')
 
-    return <h1>{greeting}</h1>
+    return (
+        <div>
+            <h1>{greeting}</h1>
+
+            <input type="text" value={greeting} onChange={event => setGreeting(event.target.value)}
+            />
+        </div>
+    );
 };
+
 
 export default Headline;
